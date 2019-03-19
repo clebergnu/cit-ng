45433b554789290efba02490537f04d71e222eca
Cleber Rosa <crosa@redhat.com>
2019-03-19 14:46:05 -0400

    Adjust test to interface changes
    The Cit class now takes data that should come from the parser.  Let's
    adjust the test to cover that.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,29 +1,16 @@
 import unittest
 
-from varianter_cit import Cit
+from varianter_cit.Cit import Cit
+from varianter_cit.Parser import Parser
 
 
 class Basic(unittest.TestCase):
 
     def test_interface(self):
-        input_data = [2, 2, 2, 2, 6, 2, 2, 2, 2, 2, 2, 2, 2,
-                      2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        with open("./data_file_example.txt") as input_file:
+            parameters, constraints = Parser.parse(input_file)
         t_value = 2
-        constraints = ['0 != 1 || 1 != 0',
-                       '4 != 4 || 5 != 1',
-                       '4 != 5 || 5 != 1',
-                       '0 != 1 || 1 != 0',
-                       '3 != 0 || 4 != 4',
-                       '3 != 1 || 4 != 5',
-                       '1 != 0 || 4 != 0',
-                       '0 != 1 || 3 != 0',
-                       '1 != 1 || 4 != 3',
-                       '1 != 1 || 2 != 0',
-                       '3 != 0 || 5 != 0',
-                       '3 != 1 || 5 != 0',
-                       '3 != 0 || 5 != 0',
-                       '0 != 0 || 4 != 2 || 5 != 1',
-                       '0 != 1 || 1 != 0 || 3 != 1']
-        program = Cit.Cit(input_data, t_value, constraints)
+        input_data = [parameter.get_size() for parameter in parameters]
+        program = Cit(input_data, t_value, constraints)
         result = program.compute()
         self.assertIsNotNone(result)
--- varianter_cit/Parameter.py
+++ varianter_cit/Parameter.py
@@ -7,6 +7,9 @@ class Pair:
     def __str__(self):
         return str(self.name) + " != " + str(self.value)
 
+    def __repr__(self):
+        return "<Pair name=%s value=%s>" % (self.name, self.value)
+
     def __eq__(self, other):
         return self.name == other.name and self.value == other.value
 
